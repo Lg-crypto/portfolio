24cdb6e25345cc1d5c4a85b3fa28fdf47c59de99
Lg-crypto <halejandroalvesoliveira10@gmail.com>
2026-07-17 16:35:43 -0300

    feat: refactor header component for responsive design, update styles, and add overlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import './App.css'
+import styles from './App.module.css'
 import Home from "./pages/home"
 import About from './pages/about'
 
 function App() {
   return (
-    <>
+    <div className={styles.app}>
       <Home></Home>
       <About></About>
-    </>
+    </div>
   )
 }
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,26 @@
 import styles from "./header.module.css"
+import { useState } from "react"
 
 export default function Header () {
+    const [open, setOpen] = useState(false);
+
+
     return(
-        <header className={styles.headerContainer}>
-            <div className={styles.logo}>HL</div>
-            <nav>
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About me</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <div className={styles.overlay} style={{display: open ? "none" : "flex"}}></div>
+            <header className={styles.headerContainer}>
+                <div className={styles.logo}>HL</div>
+                <div className={styles.menuIcon} onClick={()=> setOpen( open ? false : true )}>
+                    <span className={`${styles.menuIconLine} + ${open ? styles.open : styles.closed}`} ></span>
+                </div>
+                <nav className={ open ? styles.closed : styles.open}>
+                    <ul>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About me</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     )
 }
